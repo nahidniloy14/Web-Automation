6c735d2df5b43ac5c4d1a1e3efec5ce7b306039c
Niloy <nahidniloy894@gmail.com>
2022-11-27 07:27:15 +0600

    27/11/22

--- main.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-book=openpyxl.load_workbook("C:\\Users\\lm\\OneDrive - American International University-Bangladesh\\Documents\\RoadToSqa.xlsx")#file path
-sheet=book.active #active excel sheet
-cell=sheet.cell(row=15,column=2)
-print(cell.value)
-sheet.cell(row=1,column=5).value="Experiment"
-print(sheet.cell(row=1,column=5).value)#value will be added to the sheet
-print(sheet.max_row)
-print(sheet.max_column)
